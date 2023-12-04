970f8dcce9605a7d72a2f1278fd05b8f47cd28c6
srs-sieam <srssieam@gmail.com>
2023-12-04 22:23:21 +0600

    display national news

--- src/components/ui/NationalNews.js
+++ src/components/ui/NationalNews.js
@@ -9,110 +9,56 @@ import img1 from "@/assets/Rectangle 9.png";
 import img2 from "@/assets/Rectangle 10.png";
 import img3 from "@/assets/Rectangle 11.png";
 import img4 from "@/assets/Rectangle 12.png";
+import { getAllNews } from '@/utils/getAllNews';
 
-const NationalNews = () => {
+const NationalNews = async () => {
+    const { data } = await getAllNews();
     return (
         <>
             <Card>
                 <CardActionArea>
                     <CardMedia>
-                        <Image src={national1} alt='national news'></Image>
+                        <Image src={data[0].thumbnail_url} width={800} height={500} alt='national news'></Image>
                     </CardMedia>
                     <CardContent>
-                        <Typography gutterBottom className='max-w-min' sx={{ color: "white", padding: "6px", borderRadius: "6px", backgroundColor: "#FF0000" }}>Technology</Typography>
+                        <Typography gutterBottom className='max-w-min' sx={{ color: "white", padding: "6px", borderRadius: "6px", backgroundColor: "#FF0000" }}>{data[0].category}</Typography>
                         <Typography gutterBottom variant="h4">
-                            Bitcoin Climbs as Elon Musk Says Tesla Likely to Accept it Again
+                            {data[0].title}
                         </Typography>
                         <Typography gutterBottom variant="body1" color="text.secondary">
-                            By Srs Sieam - Mar 18 2023
+                            By {data[0].author.name} - {data[0].author.published_date}
                         </Typography>
                         <Typography variant="body1" color="text.secondary">
-                            It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout........
+                            {data[0].details.length > 200 ? data[0].details.slice(0, 200) + "..." : data[0].details}
                         </Typography>
                     </CardContent>
                 </CardActionArea>
             </Card>
             <Grid className='my-11' container rowSpacing={3} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
-                <Grid item xs={6}>
-                    <Card>
-                        <CardActionArea>
-                            <CardMedia>
-                                <Image src={img1} alt='national news'></Image>
-                            </CardMedia>
-                            <CardContent>
-                                <Typography className='text-xs md:text-2xl' gutterBottom variant="h5">
-                                    EU’s New Sanctions Aim to Cut Russia Off From World Bank
-                                </Typography>
-                                <Typography className='text-xs md:text-xl' gutterBottom variant="body1" color="text.secondary">
-                                    By Srs Sieam - Mar 18 2023
-                                </Typography>
-                                <Typography className='text-xs md:text-xl' color="text.secondary">
-                                    It is a long established fact that a reader will be distracted by the readable.....
-                                </Typography>
-                            </CardContent>
-                        </CardActionArea>
-                    </Card>
-                </Grid>
-                <Grid item xs={6}>
-                    <Card>
-                        <CardActionArea>
-                            <CardMedia>
-                                <Image src={img2} alt='national news'></Image>
-                            </CardMedia>
-                            <CardContent>
-                                <Typography className='text-xs md:text-2xl' gutterBottom variant="h5">
-                                    Battle Analysis: Russian Tank Armada vs Ukraine
-                                </Typography>
-                                <Typography className='text-xs md:text-xl' gutterBottom variant="body1" color="text.secondary">
-                                    By Srs Sieam - Mar 18 2023
-                                </Typography>
-                                <Typography className='text-xs md:text-xl' color="text.secondary">
-                                    It is a long established fact that a reader will be distracted by the readable.....
-                                </Typography>
-                            </CardContent>
-                        </CardActionArea>
-                    </Card>
-                </Grid>
-                <Grid item xs={6}>
-                    <Card>
-                        <CardActionArea>
-                            <CardMedia>
-                                <Image src={img3} alt='national news'></Image>
-                            </CardMedia>
-                            <CardContent>
-                                <Typography className='text-xs md:text-2xl' gutterBottom variant="h5">
-                                    France’s Largest Business Summit to Conclude on Mar 22
-                                </Typography>
-                                <Typography className='text-xs md:text-xl' gutterBottom variant="body1" color="text.secondary">
-                                    By Srs Sieam - Mar 18 2023
-                                </Typography>
-                                <Typography className='text-xs md:text-xl' color="text.secondary">
-                                    It is a long established fact that a reader will be distracted by the readable.....
-                                </Typography>
-                            </CardContent>
-                        </CardActionArea>
-                    </Card>
-                </Grid>
-                <Grid item xs={6}>
-                    <Card>
-                        <CardActionArea>
-                            <CardMedia>
-                                <Image src={img4} alt='national news'></Image>
-                            </CardMedia>
-                            <CardContent>
-                                <Typography className='text-xs md:text-2xl' gutterBottom variant="h5">
-                                    Marquez Explains Lack of Confidence During Qatar GP
-                                </Typography>
-                                <Typography className='text-xs md:text-xl' gutterBottom variant="body1" color="text.secondary">
-                                    By Srs Sieam - Mar 18 2023
-                                </Typography>
-                                <Typography className='text-xs md:text-xl' color="text.secondary">
-                                    It is a long established fact that a reader will be distracted by the readable.....
-                                </Typography>
-                            </CardContent>
-                        </CardActionArea>
-                    </Card>
-                </Grid>
+                {
+                    data.slice(0, 4).map(news => (
+                        <Grid key={news.id} item xs={6}>
+                            <Card>
+                                <CardActionArea>
+                                    <CardMedia sx={{ "& img": { width: "100%", height: "250px" } }}>
+                                        <Image src={news.thumbnail_url} width={800} height={300} alt='national news'></Image>
+                                    </CardMedia>
+                                    <CardContent>
+                                        <Typography className='text-xs md:text-2xl' gutterBottom variant="h5">
+                                            {news.title}
+                                        </Typography>
+                                        <Typography className='text-xs md:text-xl' gutterBottom variant="body1" color="text.secondary">
+                                            By Srs Sieam - Mar 18 2023
+                                        </Typography>
+                                        <Typography className='text-xs md:text-xl' color="text.secondary">
+                                            It is a long established fact that a reader will be distracted by the readable.....
+                                        </Typography>
+                                    </CardContent>
+                                </CardActionArea>
+                            </Card>
+                        </Grid>
+                    ))
+                }
             </Grid>
         </>
     );
